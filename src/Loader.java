edf56550a605ecdd0185577858333b01439ca307
MaDMikeNsk <mikemenshikov85@gmail.com>
2019-01-29 15:30:35 +0300

    1

--- src/Loader.java
+++ src/Loader.java
@@ -1,23 +1,25 @@
 
+//С помощью методов indexOf(), lastIndexOf(), substring() и trim() написать код в проекте StringExperiments, который считает сумму заработка Васи и Маши.
+
+//"Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей"
+
 public class Loader
 {
     public static void main(String[] args)
     {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        String text2 = "     sfsafwefd  54656   kljk  0-000987 644     ";
+        int i=0;
+        int ch;
+        char j;
+        String number;
 
-        char ch = text.charAt(2);
-        int chCode = (int)ch;
-        System.out.println(chCode);
-        System.out.println(ch);
-        System.out.println(text);
-        System.out.println("Text length: " + text.length());
-        System.out.println(text.lastIndexOf("руб"));
+//        do {
+//            ch = text.indexOf(i);
+//            j = (char)ch;
+//            if ()
+//
+//        } while (i<=text.length());
 
-        String fragment = text.substring(62,65);
-        System.out.println(fragment);
-        System.out.println(text2.trim());
-
-
+        System.out.println(text.indexOf(7));
     }
 }
