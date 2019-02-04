e11330a2268be5cd5dc18eae536a1f5dd1960b7f
MaDMikeNsk <mikemenshikov85@gmail.com>
2019-02-04 08:43:27 +0300

    very wet code :)

--- src/Loader.java
+++ src/Loader.java
@@ -4,39 +4,32 @@
 
 //"Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей"
 
+import org.jetbrains.annotations.NotNull; //IDEA порекомендовала
+
 public class Loader
 {
-    private static Integer findAmountInText (String text, int startPoint)
+    private static Integer reverseParseTextFrom(@NotNull String text, int startPoint)
     {
         char ch;
         Integer a;
-        String str = " ";
-        do
-            {
-                startPoint--;
-                ch = text.charAt(startPoint);
-                if (((48<=(int)ch)&&((int)ch<=57)) || (int)ch==32) {
-                    str = ch + str;
-                }
-            } while (((48<=(int)ch)&&((int)ch<=57)) || (int)ch==32);
+        String str = "";
+        do {
+            startPoint--;
+            ch = text.charAt(startPoint);
+            if (((48<=(int)ch)&&((int)ch<=57)) || (int)ch==32) { // код '0' - 48, код '9' - 57, space - 32
+                str = ch + str;
+            }
+        } while (((48<=(int)ch)&&((int)ch<=57)) || (int)ch==32);
         a = Integer.parseInt(str.trim());
-            return a;
+        return a;
     }
-
-// код '0' - 48
-//код '9' - 57
-// space - 32
-
-//первое вхождение - 20
-//последнее вхождение - 62
-
 
     public static void main(String[] args)
     {
-        String text = "Вася заработал 858 рублей, Петя - 7563 рубля, а Маша - 2 рублей";
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
         String str = "руб";
-        int vasya = findAmountInText(text,text.indexOf(str));
-        int masha = findAmountInText(text,text.lastIndexOf(str));
+        int vasya = reverseParseTextFrom(text,text.indexOf(str));
+        int masha = reverseParseTextFrom(text,text.lastIndexOf(str));
         System.out.println("Зарплата Васи и Маши: " + (vasya + masha));
     }
 }
